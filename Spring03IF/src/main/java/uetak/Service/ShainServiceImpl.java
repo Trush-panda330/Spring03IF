20be031357ab5181fbff5d6f7b46ccff7d6a771e
uetak330 <uetak330@gmail.com>
2024-09-29 16:55:07 +0900

    ShainServiceImplクラスの作成

---
+++ Spring03IF/src/main/java/uetak/Service/ShainServiceImpl.java
@@ -0,0 +1,15 @@
+package uetak.Service;
+
+import java.util.ArrayList;
+
+import uetak.entity.Shain;
+
+public class ShainServiceImpl implements ShainService {
+
+	@Override
+	public ArrayList<Shain> findAll() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
