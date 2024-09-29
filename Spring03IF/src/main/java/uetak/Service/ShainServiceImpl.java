37fc0e0c0dc3a4e5f8d01da99545b8f3aea2adf1
uetak330 <uetak330@gmail.com>
2024-09-29 16:58:11 +0900

    中身の実装

--- Spring03IF/src/main/java/uetak/Service/ShainServiceImpl.java
+++ Spring03IF/src/main/java/uetak/Service/ShainServiceImpl.java
@@ -2,14 +2,23 @@ package uetak.Service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import uetak.Repository.ShainRepository;
 import uetak.entity.Shain;
 
+@Service
 public class ShainServiceImpl implements ShainService {
-
+	
+	@Autowired
+	ShainRepository shainRepository;
+	
+	
 	@Override
 	public ArrayList<Shain> findAll() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		//社員リストを返す
+		return shainRepository.findAll();
 	}
 
 }
